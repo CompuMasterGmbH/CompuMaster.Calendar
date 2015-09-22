70455a3cddfa6b706cc55f2ca0be7e736aff47bf
Jochen Wezel <wezel@wks08.lan.compumaster.de>
2015-09-22 09:33:59 +0200

    Change to MIT license

--- CompuMaster.Calendar/AssemblyInfo.vb
+++ CompuMaster.Calendar/AssemblyInfo.vb
@@ -2,17 +2,17 @@ Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyTitle("CompuMaster.Calendar - LGPL 2.1 edition")>
+<Assembly: AssemblyTitle("CompuMaster.Calendar - MIT license edition")>
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("CompuMaster GmbH")> 
 <Assembly: AssemblyProduct("camm Tools")> 
-<Assembly: AssemblyCopyright("2005-2013 CompuMaster GmbH")> 
+<Assembly: AssemblyCopyright("2005-2015 CompuMaster GmbH")> 
 <Assembly: AssemblyTrademark("camm")> 
 <Assembly: CLSCompliant(True)> 
+<Assembly: ComVisibleAttribute(False)> 
+<Assembly: System.Security.AllowPartiallyTrustedCallers()> 
 
-'Die folgende GUID ist für die ID der Typbibliothek, wenn dieses Projekt in COM angezeigt wird
 <Assembly: Guid("5F847B21-B1CA-4D9E-81CB-8BDE9D59978C")> 
 
-
-<Assembly: AssemblyVersion("2012.10.30.100")> 
+<Assembly: AssemblyVersion("2015.09.22.100")> 
 <Assembly: AssemblyDelaySign(False)>
