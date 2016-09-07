b0d490e20cf56094a9b07a8ebcf2732c39e2ec00
Jochen Wezel <jwezel@compumaster.de>
2016-09-07 13:22:28 +0200

    renamed .nuspec file for content to take effect on nuget pack

--- CompuMaster.Calendar/AssemblyInfo.vb
+++ CompuMaster.Calendar/AssemblyInfo.vb
@@ -14,5 +14,5 @@ Imports System.Runtime.InteropServices
 
 <Assembly: Guid("5F847B21-B1CA-4D9E-81CB-8BDE9D59978C")>
 
-<Assembly: AssemblyVersion("2016.03.31.100")>
+<Assembly: AssemblyVersion("2016.09.07.100")>
 <Assembly: AssemblyDelaySign(False)>
