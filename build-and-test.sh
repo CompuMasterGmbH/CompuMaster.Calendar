77e0a80ce4effe5570c9851c4f9a9f7ea9ff6d30
Jochen Wezel <jwezel@compumaster.de>
2021-03-02 17:50:58 +0100

    another trial for text execution on github workflow

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,6 +1,8 @@
 nuget restore CompuMaster.Calendar.VS2012.sln
-nuget install NUnit.ConsoleRunner -Version 3.11.1
+nuget install NUnit.ConsoleRunner -Version 3.12.0
 #msbuild /p:Configuration=Debug 
 msbuild /p:Configuration=Debug /p:Platform="Any CPU" /p:PostBuildEvent="" CompuMaster.Calendar.VS2012.sln
 #msbuild /p:Configuration=Release /p:Platform="Any CPU" /p:PostBuildEvent="" CompuMaster.Calendar.VS2012.sln
-mono ./NUnit.ConsoleRunner.3.11.1/tools/nunit3-console.exe ./CompuMaster.Test.Calendar/bin/CompuMaster.Test.Calendar.dll
+curl https://raw.githubusercontent.com/jochenwezel/nunit-transforms-to-junit-gitlab-compatible/master/nunit3-junit/nunit3-junit.xslt > nunit3-junit.xslt
+mono ./NUnit.ConsoleRunner.3.12.0/tools/nunit3-console.exe ./CompuMaster.Test.Calendar/bin/CompuMaster.Test.Calendar.dll --result=TestResult.xml "--result=TestResult.JUnit.xml;transform=nunit3-junit.xslt"
+rm nunit3-junit.xslt
