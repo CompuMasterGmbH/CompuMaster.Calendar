c2aeec6f828268542ec36de302381bf678e6a933
Thomas Schädler <tschaedler@compuamster.de>
2021-04-15 12:00:48 +0200

    add multiple target frameworks
    - add new .net core project (CompuMaster.Calendar_v02)
    - add targetframewors (netstandard2.0;netcoreapp3.1;net5.0;net48;net45)
    - add dependencies to Microsoft.VisualBasic

---
+++ CompuMaster.Calendar_v02/AssemblyInfo.vb
@@ -0,0 +1,13 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTrademark("camm")> 
+<Assembly: CLSCompliant(True)> 
+<Assembly: ComVisibleAttribute(False)> 
+<Assembly: System.Security.AllowPartiallyTrustedCallers()> 
+
+<Assembly: Guid("5F847B21-B1CA-4D9E-81CB-8BDE9D59978C")>
+<Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("CompuMaster.Test.Calendar")>
+
+<Assembly: AssemblyDelaySign(False)>
---
+++ CompuMaster.Calendar_v02/WeekNumberSpan.vb
@@ -0,0 +1,48 @@
+﻿Option Explicit On
+Option Strict On
+
+Namespace CompuMaster.Calendar
+
+    ''' <summary>
+    ''' A structure providing delta information between 2 WeekNumber objects
+    ''' </summary>
+    Public Class WeekNumberSpan
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(years As Integer, weeks As Integer)
+            Me.Years = years
+            Me.Weeks = weeks
+        End Sub
+
+        Public Property Years As Integer
+        Public Property Weeks As Integer
+
+        Public Overrides Function Equals(obj As Object) As Boolean
+            If obj IsNot Nothing AndAlso obj.GetType IsNot Me.GetType Then
+                Throw New ArgumentException(NameOf(obj))
+            End If
+            Dim tObj As WeekNumberSpan = CType(obj, WeekNumberSpan)
+            If tObj.Years = Me.Years AndAlso tObj.Weeks = Me.Weeks Then
+                Return True
+            Else
+                Return False
+            End If
+        End Function
+
+        Public Overrides Function GetHashCode() As Integer
+            Return (Years * 100 + Weeks).GetHashCode
+        End Function
+
+        Public Shared Operator =(left As WeekNumberSpan, right As WeekNumberSpan) As Boolean
+            Return left.Equals(right)
+        End Operator
+
+        Public Shared Operator <>(left As WeekNumberSpan, right As WeekNumberSpan) As Boolean
+            Return Not left = right
+        End Operator
+
+    End Class
+
+End Namespace
